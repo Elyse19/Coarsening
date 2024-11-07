98d8f6a384beb45d82ba191a890c86beeccbbff1
Elyse19 <elyse.gliott@gmail.com>
2024-11-07 14:19:00 +0100

    Implementation of optimization (rfft, jit...) and disorder averaging

--- 1d_coarsening.py
+++ 1d_coarsening.py
@@ -3,15 +3,12 @@
 """
 Created on Fri Oct  6 15:43:11 2023
 
-@author: Nicolas Cherroret
+@author: egliott
 """
-
-# from IPython import get_ipython
-# get_ipython().magic('reset -sf')
 
 import numpy as np
 import scipy
-from scipy.fft import fft2, ifft2, fft, ifft
+from scipy.fft import rfft, irfft
 import matplotlib.pyplot as plt
 import time
 import scipy.signal
@@ -19,14 +16,12 @@ import scipy.integrate as integrate
 from scipy.ndimage import gaussian_filter
 from joblib import Parallel, delayed
 from photutils.profiles import RadialProfile
+from numba import jit
 
-fig1, axs1 = plt.subplots(1,1)
-
-start = time.time()
 n_c = 1
 
-tmax = 500 #maximum time
-dt = 0.0001 # time step
+tmax = 30 #maximum time
+dt = 10**-4 # time step
 dt2 = dt**2
 
 imbalance = 0.0 #-0.4 # initial imbalance between the two species (0 = balanced mixture)
@@ -46,57 +41,84 @@ phi2 = np.linspace(0,Nt*dt,Nt) # second moment of order parameter vs time
 
 xmax = 150
 
-Nx = 400  #number of grid points along x
+Nx = 800  #number of grid points along x
 
-
-x = np.linspace(0,xmax,Nx)
-
+x = np.linspace(0,xmax*(1-1/Nx),Nx)
 
 dx = x[1] - x[0]
 
 
-kx = np.fft.fftfreq(Nx, d = dx)*2.*np.pi
+k = np.fft.rfftfreq(Nx, d = dx)*2.*np.pi
+k_2 = k**2
+dt2_k_2 = k_2*dt2
 
+@jit(nopython=True, parallel=True)
+def compute_F(u_1_loc):
+    u1_loc_2 = u_1_loc**2
+    u1_loc_3 = u1_loc_2*u_1_loc
+    u1_loc_4 = u1_loc_2**2
+    F = (np.pi/2 - u_1_loc - 81*np.pi*u1_loc_2/238 + 367*u1_loc_3/714 + 183*np.pi*u1_loc_4/9520)/(1 - 81*u1_loc_2/119 + 183*u1_loc_4/4760)
+    return F
+#Pade approximation for arccos(phi)
+#Acceleration with jit (power calculations) and parallelization
 
+@jit(nopython=True, parallel=True)
+def sqrt_NL(u_1_loc):
+    u1_loc_2 = u_1_loc**2 
+    return np.sqrt((1 - u_1_loc**(2 + 2*Nexp))/(1 - u1_loc_2))
+#Sqrt part of the nonlinear part
+#Acceleration with jit (power calculations) and parallelization
 
-def advance_vectorized(u, u_1, u_2, u_hat, u_1_hat, u_2_hat, dt2, step1=False):
+def advance_vectorized_step1(u_1_loc):
+    dt2_prime = 0.5*dt2
     
-    if step1:
-        dt2 = 0.5*dt2  # redefine for the first time step
-        D1 = 1.
-        D2 = 0
-    else:
-        D1 = 2.
-        D2 = 1.
+    u_1_hat = rfft(u_1_loc)
     
-    u_1_hat[:] = fft(u_1)
-    u_2_hat[:] = fft(u_2)
+    F = compute_F(u_1_loc)
+    F_hat = rfft(F)
     
-    F = (np.pi/2 - u_1 - 81*np.pi*u_1**2/238 + 367*u_1**3/714 + 183*np.pi*u_1**4/9520)/(1 - 81*u_1**2/119 + 183*u_1**4/4760)
+    NL_part = sqrt_NL(u_1_loc)*irfft(k_2*F_hat)
+    NL_part_hat = rfft(NL_part)
     
-    q_2 = kx**2
     
-    NLpart_hat = fft(np.sqrt((1 - u_1**(2 + 2*Nexp))/(1 - u_1**2))*ifft(q_2*fft(F)))
-    u_hat[:] = (D1*u_1_hat - D2*u_2_hat + dt2*q_2*NLpart_hat)/(1 + (C2)*dt2*q_2) 
+    u_hat = (1*u_1_hat + dt2_prime*k_2*NL_part_hat)/(1 + C2*dt2_prime*k_2) 
+    u = irfft(u_hat).real
+
+    return u
+
+def advance_vectorized(u_1_loc, u_2_loc):
     
-    u[:] = ifft(u_hat).real
+    u_1_hat = rfft(u_1_loc)
+    u_2_hat = rfft(u_2_loc)
+    
+    F = compute_F(u_1_loc)
+    F_hat = rfft(F)
+    
+    NL_part = sqrt_NL(u_1_loc)*irfft(k_2*F_hat)
+    NL_part_hat = rfft(NL_part)
+    
+    
+    u_hat = (2*u_1_hat - u_2_hat + dt2_k_2*NL_part_hat)/(1 + C2*dt2_k_2) 
+    u = irfft(u_hat).real
 
     return u
 
 def g_1_avant_moy(phi_loc):
-    fourier = fft(phi_loc)*np.conjugate(fft(phi_loc))
-    res = ifft(fourier).real
+    phi_hat = rfft(phi_loc)
+    fourier = phi_hat*np.conjugate(phi_hat)
+    res = irfft(fourier).real
     return res
 
 ## Generation of the initial random noise
-
-correlation_scale = 3
+sig = 1
+correlation_scale = sig/dx
 
 
 n_realization = n_c
 
 int_save = int(1/dt)*10
 int_g1_save = int(0.1/dt)
+int_phi2_save = int(0.05/dt)
 saving = int(100/dt)
 
 
@@ -104,40 +126,30 @@ u = np.zeros((Nx), dtype=np.float64) # solution array
 u_1 = np.zeros((Nx), dtype=np.float64) # solution at t-dt
 u_2 = np.zeros((Nx), dtype=np.float64) # solution at t-2*dt
 
-u_hat = np.zeros((Nx), dtype=np.complex128)
-u_1_hat = np.zeros((Nx), dtype=np.complex128)
-u_2_hat = np.zeros((Nx), dtype=np.complex128)
+def ini_u(seed):   
+    np.random.seed(seed)
+    lamb_pref = eps*(np.sqrt(6)/np.pi**(1/4))/np.sqrt(sig*xmax)
+    
+    noise = np.random.rand(Nx)*2 -1
+    noise = lamb_pref*gaussian_filter(noise,correlation_scale, mode = 'wrap',truncate = 8.0)*np.sqrt(Nx*2*np.pi*sig**2)
+    
+    return noise
 
-n_i = 6
 
-np.random.seed(n_i)
 
-noise = np.random.rand(Nx)*2 -1
-noise = eps*gaussian_filter(noise,correlation_scale)
 
-# axs1.plot(x,noise)
-## Definition of initial state
+
 
 ini = 'speckle'
-u_1_in = imbalance + noise
 
+n_real = 20
 
-u_1 = u_1_in
-
-u_1_in_hat = fft(u_1_in)
-
-
-n_i = 6
-
-n = 0 # Special formula for first time step
-u = advance_vectorized(u, u_1, u_2, u_hat, u_1_hat, u_2_hat, dt2, step1=True)
-u_2, u_1, u = u_1, u, u_2
 
 u_arr = np.zeros((int(Nt/int_save)+1,Nx))
-phi2 = [np.sum(u_1**2)/(Nx)]
+# phi2 = [np.sum(u_1**2)/(Nx)]
+Cons_N0 = np.sum(u_1)/(Nx)**2 #Initial conservation number
 
-
-path = '/users/jussieu/egliott/Documents/Coarsening/2D_code/Data_coarsening/'
+path = '/users/jussieu/egliott/Documents/Coarsening/2D_code/Data_coarsening_1D/'
 
 name = 'testing_1d'
 file_name = f"_dt={dt}_Tmax={tmax}_Nx={Nx}_Xmax={xmax}_Initial={ini}_Imb={imbalance}_Eps={eps}_C={C2}_Nexp={Nexp}_seed={n_i}"
@@ -153,34 +165,57 @@ g1_save = np.zeros((int(Nt/int_g1_save)+1,Nx))
 j = 0
 k = 0
 
-for i in range(Nt):
-    t_i = t_i + dt
-    t_data.append(t_i)
+start = time.time()
+
+phi_2 = np.zeros((int(Nt/int_phi2_save) + 1))
+
+def real(n_real_i):
+    t_i = 0
+
+    u_2 = np.zeros((Nx), dtype=np.float64)
+    u_1 = ini_u(n_real_i)
     
-    if i% int_save == 0:
-        print('t = ' + str(round(t_i,3)))
-        u_arr[j] = u_1
-        j += 1
-    if i% int_g1_save == 0:
-        g1_t = ifftshift(g_1_avant_moy(u_1))
-        # g1_func = interpolate.interp1d(x,g1_t, fill_value = 'extrapolate')
-        g1_save[k] = g1_t
-        k += 1
-    
-    # print(i)
-    # Cons_N = (Cons_N0 - np.sum(u_1)/(Nx)**2)/Cons_N0
-  
-    phi2.append(np.sum(u_1**2)/(Nx))   
-    if i%saving == 0:
-        np.savez(path_save,u_arr,phi2,g1_save,t_data)
-        print(i)
-        # plt.plot(x,u_1)
-        plt.show()
-    u = advance_vectorized(u, u_1, u_2, u_hat, u_1_hat, u_2_hat, dt2)
+    u = advance_vectorized_step1(u_1)
     u_2, u_1, u = u_1, u, u_2
+    # t_i = t_i + dt
     
-np.savez(path_save,u_arr,phi2,g1_save,t_data)  
-# axs1.plot(x,u_1)
+    for i in range(Nt):
+        t_i = t_i + dt
+        
+        # if i% int_save == 0:
+        #     print('t = ' + str(round(t_i,3)))
+        #     u_arr[j] = u_1
+        #     j += 1
+        # if i% int_g1_save == 0:
+        #     g1_t = ifftshift(g_1_avant_moy(u_1))
+        #     # g1_func = interpolate.interp1d(x,g1_t, fill_value = 'extrapolate')
+        #     g1_save[k] = g1_t
+        #     k += 1
+        if i%int_phi2_save == 0:
+    
+            Cons_N = (Cons_N0 - np.sum(u_1)/(Nx)**2)/Cons_N0
+            # phi2.append(np.sum(u_1**2)/(Nx)) 
+            # t_data.append(t_i)
+            i_save = i//int_phi2_save
+            phi_2[i_save] += np.var(u_1)/(n_real + 1)
+            if n_real_i == 0:
+                t_data.append(t_i)
+        
+        if i%int_save == 0:
+            # np.savez(path_save,u_arr,phi2,g1_save,t_data)
+            print(i//int_save)
+            
+        u = advance_vectorized(u_1, u_2)
+        u_2, u_1, u = u_1, u, u_2
+        
+for n_r_i in range(n_real):
+    print('real=', n_r_i)
+    real(n_r_i)
+        
+# t_data = np.arange(0,Nt,dt)    
+t_data = np.array(t_data)
+np.savez(path_save,u_arr,phi_2,g1_save,t_data)  
+
 
 end = time.time()
 print('Time = ' + str(end -start))
